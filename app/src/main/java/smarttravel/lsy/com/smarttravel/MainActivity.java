5350391d573d9a509e464ac7bf9d383e9b648206
ZsxhLyqz <H867331878@gmailcom>
2019-11-18 22:18:07 +0800

    页面实现

---
+++ app/src/main/java/smarttravel/lsy/com/smarttravel/MainActivity.java
@@ -0,0 +1,13 @@
+package smarttravel.lsy.com.smarttravel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
